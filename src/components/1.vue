0df1ca09c4472148678949dabe19534efa11cf18
moonimooni <munhw1408@gmail.com>
2021-09-27 04:00:19 +0900

    연습용 컴포넌트 템플릿 구성

--- src/components/1.vue
+++ src/components/1.vue
@@ -1,14 +1,50 @@
 <template>
   <form action="">
     <input type="text" />
-    <input type="checkbox" value="ㅎㅏ..." name="ha" />
-    <input type="checkbox" value="프론트.. 어렵,," name="hoo" />
-    <input type="submit" value="제출" />
+    <input
+      class="search"
+      type="search"
+      :placeholder="placeholder"
+      @click="onLoad"
+      @keydown.prevent.enter="onSubmit"
+      @keydown.prevent.up="onPrev"
+      @keydown.prevent.down="onNext"
+    />
+    <div class="dropdown">
+      <a
+        href="#"
+        class="item"
+        :class="active"
+        v-for="(v, i) in this.list"
+        :key="i.id"
+      >
+        {{ getCityName(v) }}
+      </a>
+    </div>
   </form>
 </template>
 
 <script>
+import * as data from "../../api/data.json";
+
 export default {
-  name: "one"
+  name: "one",
+  data() {
+    return {
+      placeholder: "여기에 엔터를 치세요",
+      active: ""
+    };
+  },
+  methods: {
+    onLoad() {
+      this.list = data;
+    },
+    onSubmit(input) {},
+    onPrev() {},
+    onNext() {},
+    getCityName(item) {
+      return `${item.name}`;
+    }
+  }
 };
 </script>
